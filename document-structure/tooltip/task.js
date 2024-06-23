78a1fcf32ca76ca97b6b20bf49933fc0c8292f96
asgat_frontend <asr213@mail.ru>
2024-06-23 12:43:57 +0300

    tooltip исправлен

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,45 +1,13 @@
-const tooltips = Array.from(document.querySelectorAll(".has-tooltip"));
-
+const tooltips = document.querySelectorAll('.has-tooltip');
 tooltips.forEach(tooltip => {
-  const elem = document.createElement('div');
-  elem.textContent = tooltip.title;
-
-  tooltip.addEventListener("click", (e) => {
-    e.preventDefault();
-    elem.classList.add("tooltip");
-    tooltip.insertAdjacentElement('beforeBegin', elem);
-    elem.style.position = "absolute";
-    elem.style.left = `${tooltip.getBoundingClientRect().left}px`;
-    elem.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
-
-    const elems = Array.from(document.querySelectorAll(".tooltip_active"));
-    const findElem = elems.find(elem => elem.classList.contains("tooltip_active"));
-
-    if (findElem === undefined) {
-      elem.classList.add("tooltip_active");
-    } else {
-      if (findElem !== elem) {
-        elem.classList.add("tooltip_active");
-      }
-      findElem.classList.remove("tooltip_active");
-    }
-  });
-
-  tooltip.addEventListener("mouseover", () => {
-    const elems = Array.from(document.querySelectorAll(".tooltip"));
-    elems.forEach(elem => {
-      if (elem.classList.contains("has-tooltip")) {
-        elem.classList.add("tooltip_active");
-      }
+    const tooltipText = tooltip.getAttribute('title').trim();
+    tooltip.addEventListener('click', (e) => {
+        e.preventDefault();
+        const tooltipElement = document.querySelector('.tooltip');
+        tooltipElement.textContent = tooltipText;
+        tooltipElement.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
+        tooltipElement.style.left = `${tooltip.getBoundingClientRect().left}px`;
+        tooltipElement.classList.add('tooltip_active');
+        document.body.appendChild(tooltipElement);
     });
-  });
-
-  tooltip.addEventListener("mouseout", () => {
-    const elems = Array.from(document.querySelectorAll(".tooltip"));
-    elems.forEach(elem => {
-      if (elem.classList.contains("has-tooltip")) {
-        elem.classList.remove("tooltip_active");
-      }
-    });
-  });
 });
